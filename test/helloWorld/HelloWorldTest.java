79cfc5772d740761ac93e35dee4e452f147303db
alejandrozucca <alejandrozucca@gmail.com>
2015-09-28 21:34:31 -0300

    Update HelloWorldTest.java

--- test/helloWorld/HelloWorldTest.java
+++ test/helloWorld/HelloWorldTest.java
@@ -2,7 +2,7 @@ package helloWorld;
 
 import uk.co.placona.helloWorld.Cronometro;
 import org.junit.Test;
-import static org.junit.Assert.AssertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class HelloWorldTest{
 
