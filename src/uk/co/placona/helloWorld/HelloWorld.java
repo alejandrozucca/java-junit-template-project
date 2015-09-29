12fba16e6da412cb3d82782f89cc55ebc0a2b9c8
alejandrozucca <alejandrozucca@gmail.com>
2015-09-28 21:23:48 -0300

    Update HelloWorld.java

--- src/uk/co/placona/helloWorld/HelloWorld.java
+++ src/uk/co/placona/helloWorld/HelloWorld.java
@@ -2,8 +2,33 @@ package uk.co.placona.helloWorld;
 
 public class HelloWorld {
 
-	public String sayHello() {
-		return "Hello World";
+private int segundos, minutos;
+	
+	public Cronometro(){
+		segundos = 0;
+		minutos = 0;
+	}
+	
+	public void reiniciar(){
+		segundos = 0;
+		minutos = 0;
+	}
+	
+	public void incrementarTiempo(){
+		
+		segundos++;		
+		if(segundos == 60){
+			minutos++;
+			segundos = 0;
+		}		
+	}
+	
+	public int mostrarMinutos(){
+		return minutos;
+	}
+	
+	public int mostrarSegundos(){
+		return segundos;
 	}
 
 }
